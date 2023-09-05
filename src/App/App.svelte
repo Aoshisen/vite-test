934c856ba02b038f8b764f47b64720cc047ad220
aoshisen <aoshisen.23@gmail.com>
2023-09-05 23:30:52 +0800

    feat: 支持svelte scss 编译

--- src/App/App.svelte
+++ src/App/App.svelte
@@ -1,1 +1,7 @@
-<h1>hello world!</h1>
+<script>
+  let name = "ass";
+</script>
+
+<h1 class="app">hello world! {name}</h1>
+
+<style lang="scss" src="./App.scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { svelteInspector } from "@sveltejs/vite-plugin-svelte-inspector";
 import typescript2 from "rollup-plugin-typescript2";
+import sveltePreProcess from "svelte-preprocess";
 
 export default defineConfig({
-  plugins: [svelte(), svelteInspector(), typescript2()],
-  resolve: {},
+  plugins: [
+    svelte({
+      preprocess: sveltePreProcess(),
+    }),
+    svelteInspector(),
+    typescript2(),
+  ],
 });
