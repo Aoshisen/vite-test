d9627390220fe9a1cdc0320703336deb13ee2e05
aoshisen <aoshisen.23@gmail.com>
2023-09-06 22:49:57 +0800

    feat: progress 插件添加

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,37 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { svelteInspector } from "@sveltejs/vite-plugin-svelte-inspector";
 import typescript2 from "rollup-plugin-typescript2";
 import sveltePreProcess from "svelte-preprocess";
-import {cssModules,linearPreprocess} from "svelte-preprocess-cssmodules"
-import autoprefixer from "autoprefixer"
+import { cssModules, linearPreprocess } from "svelte-preprocess-cssmodules";
+import autoprefixer from "autoprefixer";
+import progress from "vite-plugin-progress";
 
 export default defineConfig({
   plugins: [
-    svelte({
-      preprocess: 
-      linearPreprocess([
-        sveltePreProcess({
-          postcss:{
-            plugins:[autoprefixer()]
-          },
-        }),
-        cssModules({
-          mode:"scoped",
-          useAsDefaultScoping:true,
-        })
-      ])
-    ,
-      compilerOptions: {
-        cssHash: ({ css, hash, name, filename }) => {
-          return `fx-${hash(css)}`;
-        },
-      },
-      emitCss: false,
-    }),
     svelteInspector(),
     typescript2(),
+    progress(),
+    svelte({
+      preprocess: linearPreprocess([
+        //线性处理css 模块化
+        sveltePreProcess({
+          postcss: {
+            plugins: [autoprefixer()],
+          },
+        }),
+        //设置默认的css为scoped
+        cssModules({
+          mode: "scoped",
+          useAsDefaultScoping: true,
+        }),
+      ]),
+      compilerOptions: {
+        //定义css 类名的格式
+        cssHash: ({ css, hash, name, filename }) => {
+          return `ass-${hash(css)}`;
+        },
+      },
+      //是否单独的生成一个css 文件(如果指定了compilerOptions 那么设置成false 的时候才会去走cssHash 的逻辑)
+      emitCss: false,
+    }),
   ],
 });
