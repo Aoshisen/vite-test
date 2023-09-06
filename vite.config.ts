1463378e729e1af782ec2ab195fa6040623e1512
aoshisen <aoshisen.23@gmail.com>
2023-09-06 22:37:44 +0800

    feat: css 模块化打包

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,31 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { svelteInspector } from "@sveltejs/vite-plugin-svelte-inspector";
 import typescript2 from "rollup-plugin-typescript2";
 import sveltePreProcess from "svelte-preprocess";
+import {cssModules,linearPreprocess} from "svelte-preprocess-cssmodules"
+import autoprefixer from "autoprefixer"
 
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: sveltePreProcess(),
+      preprocess: 
+      linearPreprocess([
+        sveltePreProcess({
+          postcss:{
+            plugins:[autoprefixer()]
+          },
+        }),
+        cssModules({
+          mode:"scoped",
+          useAsDefaultScoping:true,
+        })
+      ])
+    ,
+      compilerOptions: {
+        cssHash: ({ css, hash, name, filename }) => {
+          return `fx-${hash(css)}`;
+        },
+      },
+      emitCss: false,
     }),
     svelteInspector(),
     typescript2(),
